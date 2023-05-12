63631adcdc5144ea1d0883362d12ea7fcf7935d4
abhishek-paghdal <abhishek.paghdal@rapidops.com>
2023-05-12 11:01:14 +0530

    Data Ingestion"

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,4 @@
-import logging
+from src.logger import logging
 import sys
 
 def error_message_detail(error, error_detail:sys):
@@ -20,3 +20,4 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message    
     
+  
